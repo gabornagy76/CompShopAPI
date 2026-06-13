879a32eb1f2cdcc85316d271c1cf9113e1f075a5
Nagy Gábor <nagyg76x@gmail.com>
2026-06-13 10:34:18 +0200

    Computers tábla és DBContext elkészült

---
+++ CompShopAPI/Models/CompShopDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CompShopAPI.Models
+{
+    public class CompShopDBContext : DbContext
+    {
+        // Hozzunk létre egy paraméterek nélküli kontruktort
+        public CompShopDBContext()
+        {
+        }
+
+        // Konstruktor a lekédezésekhez
+        public CompShopDBContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        // Függvény a kapcsolat létrehozásához
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("SERVER=localhost;DATABASE=CompShop;Uid=root;PWD=");
+        }
+    }
+}
---
+++ CompShopAPI/Models/Computers.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompShopAPI.Models
+{
+    public class Computers
+    {
+        // Elsődleges kulcs, kell hozzá a DataAnnotations névtér. Nem kötelező, az EF automatikusan kikövetkezteti (ID névből, vagy az osztály nevével egyező mezőnévből)
+        [Key]
+        public int Id {  get; set; }
+
+        // VARCHAR(50) - gyártó
+        [MaxLength(50)]
+        public string Brand { get; set; }
+
+        // Típus
+        [MaxLength(150)]
+        public string Type { get; set; }
+
+        // Képátló
+        public double Display {  get; set; }
+
+        // A rekord kreálásának ideje
+        public DateTime CreatedAt { get; set; }
+
+        // A rekord módosításának ideje
+        public DateTime UpdatedAt { get; set; }
+    }
+}
